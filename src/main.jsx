3f6be208c8ba9600d69fe30b1cf7842735597ba7
arman-borkhani <armanborkhani@gmail.com>
2026-01-05 12:13:35 +0330

    Import marker images

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "leaflet/dist/leaflet.css"
+import L from 'leaflet'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+import 'leaflet/dist/leaflet.css'
 import './index.css'
 import App from './App.jsx'
+
+// Fix for default markers in production builds
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
